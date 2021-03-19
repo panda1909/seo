97a0316c08d4042c646a70c720a54cfefadb57d0
Muhammad Umair <Umairmuhammad433@gmail.com>
2021-03-19 17:41:28 +0500

    heroku live

--- seo_project/settings.py
+++ seo_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 
@@ -123,3 +123,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
